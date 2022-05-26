c668dcb9dd635639fac2d96c63a861fe17d072c5
Louis Brandt <mail@louisbrandt.xyz>
2022-05-26 09:17:20 +0200

    mono emb training

--- scripts/emb_train.py
+++ scripts/emb_train.py
@@ -10,14 +10,14 @@ def dd():
   return defaultdict(dd2)
 
 def main():
-  languages = ['jp']
-  with open('../data/all_data.pickle', 'rb') as f:
+  languages = ['fr']
+  with open('../data/amazon_reviews/all_data.pickle', 'rb') as f:
     data = pickle.load(f)
   print('data loaded')
 
   for lang in languages:
-    corpus = data[lang]['train']['corpus'] + data[lang]['test']['corpus']
-    model = Word2Vec(corpus,min_count=5, vector_size=200, workers=5, window=4, sg=1)
+    corpus = data[lang]['train']['corpus'][0:100] + data[lang]['test']['corpus'][0:100]
+    model = Word2Vec(corpus,min_count=5, vector_size=200, workers=5, window=4,sg=1,negative=5)
     model.train(corpus,total_examples=model.corpus_count,epochs=50)
     print(lang, ' trained!')
 
