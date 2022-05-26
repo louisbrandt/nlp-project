a30315811fe333e95f7d77d4b26099063283eb1d
Louis Brandt <mail@louisbrandt.xyz>
2022-05-26 09:42:26 +0200

    callback

--- scripts/emb_train.py
+++ scripts/emb_train.py
@@ -2,12 +2,26 @@ import gensim
 from gensim.models import Word2Vec 
 import pickle
 from collections import defaultdict
- 
+from gensim.test.utils import common_texts, get_tmpfile
+from gensim.models import Word2Vec
+from gensim.models.callbacks import CallbackAny2Vec
+
 def dd2():
   return dict()
 
 def dd():
   return defaultdict(dd2)
+
+class callback(CallbackAny2Vec):
+    '''Callback to print loss after each epoch.'''
+
+    def __init__(self):
+        self.epoch = 0
+
+    def on_epoch_end(self, model):
+        loss = model.get_latest_training_loss()
+        print('Loss after epoch {}: {}'.format(self.epoch, loss))
+        self.epoch += 1
 
 def main():
   languages = ['fr']
@@ -16,9 +30,9 @@ def main():
   print('data loaded')
 
   for lang in languages:
-    corpus = data[lang]['train']['corpus'][0:100] + data[lang]['test']['corpus'][0:100]
-    model = Word2Vec(corpus,min_count=5, vector_size=200, workers=5, window=4,sg=1,negative=5)
-    model.train(corpus,total_examples=model.corpus_count,epochs=50)
+    corpus = data[lang]['train']['corpus'][0:30000] + data[lang]['test']['corpus'][0:30000]
+    model = Word2Vec(corpus,min_count=5, vector_size=200, workers=5, window=4,sg=1,negative=5,callbacks=[callback()])
+    model.train(corpus,total_examples=model.corpus_count,epochs=20)
     print(lang, ' trained!')
 
     with open(f'../embeddings/mono/{lang}.pickle','wb') as x:
